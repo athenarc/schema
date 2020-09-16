fa921651c44e0b2aab434a1c5597440eb7af1873
Ubuntu <zagganas@athenarc.gr>
2020-09-16 19:28:01 +0300

    Removed covid19 data requests

--- views/filebrowser/index.php
+++ views/filebrowser/index.php
@@ -20,10 +20,10 @@ foreach ($messages as $message)
 }
 ?>
 <div class="row">&nbsp;</div>
-<div class="alert alert-warning" role="alert">
+<!-- <div class="alert alert-warning" role="alert">
 	<div class="row"><div class="col-md-12">In response to the COVID-19 outbreak, we provide a range of datasets that can be used by researchers working on SARS-CoV-2 research. If you are interested to load these datasets to your home folder, then click on the "Get COVID-19 data" button. You can anytime modify or remove the files using the file browser interface.</div></div>
 	<div class="row"><div class="col-md-12 text-center covid-button-container"><?=Html::a("$covidButtonIcon Get COVID-19 data",['/filebrowser/get-covid-data'],['class'=>'btn btn-info'])?>&nbsp;&nbsp;<?=Html::a("$covidDatasetRequest Request new dataset",['/filebrowser/request-dataset'],['class'=>'btn btn-primary'])?></div></div>
-</div>
+</div> -->
 <div class="row">&nbsp;</div>
 
 <?= alexantr\elfinder\ElFinder::widget([
