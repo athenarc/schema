5cd295122f505d676359a2a65a03524d171d974b
Ubuntu <zagganas@athenarc.gr>
2020-12-02 14:33:30 +0200

    Made changes to upload and delete software, since we are using a k8s-hosted registry

--- scheduler_files/imageRemover.py
+++ scheduler_files/imageRemover.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import subprocess
 import psycopg2 as psg
@@ -6,6 +6,8 @@ import requests
 import json
 from requests.auth import HTTPBasicAuth
 import os
+import time
+import uuid
 
 
 configFileName=os.path.dirname(os.path.abspath(__file__)) + '/configuration.json'
@@ -41,15 +43,44 @@ cur.execute(sql)
 results=cur.fetchone()
 
 count=results[0]
+count=1
 if count==1:
-    image=imageName + '-' + imageVersion
+    image=imageName.lower() + '-' + imageVersion.lower()
 
+    ##
+    # If there is a process is uploading, wait for it to finish
+    #
+    sql="SELECT COUNT(*) FROM operation_locks"
+    cur.execute(sql)
+    results=cur.fetchone()
+    opCount=results[0]
+    while opCount>0:
+        time.sleep(5)
+        sql="SELECT COUNT(*) FROM operation_locks"
+        cur.execute(sql)
+        results=cur.fetchone()
+        opCount=results[0]
 
+    ##
+    # Add a lock in the database
+    ##
+    uniqid=uuid.uuid4()
+    uniqid=str(uniqid)
+    sql="INSERT INTO operation_locks(id,operation) VALUES ('" + uniqid + "','image_delete')"
+    cur.execute(sql)
+    conn.commit()
     #######get the manifest in openstack
     url='https://' + registry + '/v2/' + image + '/manifests/latest'
     headers={'Accept':'application/vnd.docker.distribution.manifest.v2+json'}
     ssl=[certsFolder + 'client.crt',certsFolder +'client.key']
-    response = requests.get(url,headers=headers, cert=ssl, verify=False, auth=HTTPBasicAuth(registryUsername, registryPassword))
+    ##
+    # Comment the following line if you are using self-signed certificates
+    ##
+    response = requests.get(url,headers=headers, verify=True, auth=HTTPBasicAuth(registryUsername, registryPassword))
+    ##
+    # Uncomment the following line if you are using self-signed certificates
+    ##
+    # response2 = requests.delete(urlDel, cert=ssl, verify=False, auth=HTTPBasicAuth(registryUsername, registryPassword))
 
     if 'Docker-Content-Digest' in response.headers:
         digest=response.headers['Docker-Content-Digest']
@@ -57,17 +88,84 @@ if count==1:
 
         print(digest)
 
+        ##
+        # get initial pod name for restart
+        ##
+        command=['kubectl', 'get', 'pods', '-n', 'registry', '--no-headers']
+        try:
+            out=subprocess.check_output(command,stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as exc:
+            print(exc.output)
+            exit(49)
+        pod=out.split()[0]
+
+
 
         ##### Remove in an openstack setting
         urlDel='https://' + registry + '/v2/' + image + '/manifests/' + digest
-        response2 = requests.delete(urlDel, cert=ssl, verify=False, auth=HTTPBasicAuth(registryUsername, registryPassword))
-        returnCode=subprocess.call('sudo juju run --unit docker-registry/0 "docker container restart registry" 2>&1', shell=True)
-        returnCode=subprocess.call('sudo juju run --unit docker-registry/0 "docker exec registry bin/registry garbage-collect /etc/docker/registry/config.yml" 2>&1', shell=True)
+        ##
+        # Comment the following line if you are using self-signed certificates
+        ##
+        response = requests.get(url,headers=headers, verify=True, auth=HTTPBasicAuth(registryUsername, registryPassword))
+        ##
+        # Uncomment the following line if you are using self-signed certificates
+        ##
+        # response2 = requests.delete(urlDel, cert=ssl, verify=False, auth=HTTPBasicAuth(registryUsername, registryPassword))
+
+        ##
+        #   Uncomment the following lines you you are using a docker registry inside the K8s cluster
+        ##
+        returnCode=subprocess.call('kubectl -n registry delete pod ' + pod.decode("utf-8") + ' 2>&1', shell=True)
+        ##
+        # get pod name after restart
+        ##
+        command=['kubectl', 'get', 'pods', '-n', 'registry', '--no-headers']
+        try:
+            out=subprocess.check_output(command,stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as exc:
+            print(exc.output)
+            exit(49)
+        pod=out.split()[0]
+
+        ##
+        # Wait until all containers in the new pod are ready
+        ##
+        status=(out.split()[1]).decode("utf-8")
+        while status!="1/1":
+            time.sleep(3)
+            command=['kubectl', 'get', 'pods', '-n', 'registry', '--no-headers']
+            try:
+                out=subprocess.check_output(command,stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as exc:
+                print(exc.output)
+                exit(49)
+            status=(out.split()[1]).decode("utf-8")
+        ##
+        # Send garbage collection command to pod
+        ##
+        returnCode=subprocess.call('kubectl -n registry exec ' + pod.decode("utf-8") + ' -- registry garbage-collect /etc/docker/registry/config.yml 2>&1', shell=True)
+
+        ##
+        #   Uncomment the following lines you you are using a docker registry in the same machine
+        ##
+        # returnCode=subprocess.call('sudo juju run --unit docker-registry/0 "docker container restart registry" 2>&1', shell=True)
+        # returnCode=subprocess.call('sudo juju run --unit docker-registry/0 "docker exec registry bin/registry garbage-collect /etc/docker/registry/config.yml" 2>&1', shell=True)
+
+        ##
+        #   Uncomment the following lines you you are using a docker registry created by juju
+        ##
+        # returnCode=subprocess.call('sudo juju run --unit docker-registry/0 "docker container restart registry" 2>&1', shell=True)
+        # returnCode=subprocess.call('sudo juju run --unit docker-registry/0 "docker exec registry bin/registry garbage-collect /etc/docker/registry/config.yml" 2>&1', shell=True)
+
+
+    sql="DELETE FROM operation_locks WHERE id='" + uniqid + "'"
+    cur.execute(sql)
+    conn.commit()
 
     print('Deleting from local')
     subprocess.call(['docker', 'rmi', registry + '/' + image + ':latest'])
 
-
+ 
 #delete from DB
 
 sql1="SELECT id FROM software WHERE name='" + imageName + "' AND version='" + imageVersion + "'"
