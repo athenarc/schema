5a4e9068368f2801100a36a068ab0bb0665bfb62
lukaskavouras <lukaskavouras@gmail.com>
2020-12-03 15:45:11 +0200

    Css removed from views

--- components/UploadDatasetModal.php
+++ components/UploadDatasetModal.php
@@ -20,32 +20,32 @@ class UploadDatasetModal
 		$model=new UploadDataset;
 
 
-
+		echo Html::cssFile('@web/css/components/uploadDataset.css');
 		$form=ActiveForm::begin(['action'=>['filebrowser/upload-dataset'], 'method'=> 'POST', 'options' => ['enctype'=>'multipart/form-data']]);//
 
 		echo "<div class='modal fade' tabindex='-1' role='dialog' id='upload-modal' aria-labelledby='upload-modal' aria-hidden='true'>";
-		echo '<div class="modal-dialog modal-dialog-centered modal-lg" role="document" style="width:600px;">';
+		echo '<div class="modal-dialog modal-dialog-centered modal-lg modal-size" role="document">';
 		echo '<div class="modal-content" >';
 		echo '<div class="modal-header">';
-		echo "<h5 class='modal-title text-center' style='font-size:25px' id='exampleModalLongTitle'> Dataset details</h5>";
+		echo "<h5 class='modal-title text-center size'  id='exampleModalLongTitle'> Dataset details</h5>";
 		echo '</div>';
 		echo '<div class="modal-body">';
-		echo  '<div class="row" style="font-size:15px; margin-bottom:10px;">
-				 <span class="col-md-4" style="margin-top:5px;">Upload dataset to: </span>
+		echo  '<div class="row body-row">
+				 <span class="col-md-4">Upload dataset to: </span>
 				 <span class="col-md-8" >'. $form->field($model,'provider')
 				 ->dropdownList($datasets, ['class'=>'form-control'])->label("").'</span>
 				</div>';
-		echo  '<div class="row" style="font-size:15px; margin-bottom:10px;">
-					<span class="col-md-4" style="margin-top:7px;"> File:</span> 
+		echo  '<div class="row body-row">
+					<span class="col-md-4"> File:</span> 
 					<span class="col-md-8">'. $form->field($model,'dataset_id')
 					->textInput(['class'=>'form-control'])->label("").'</span>
 				</div>';
-		echo  '<div class="row" style="font-size:15px; margin-bottom:10px;">
-					<span class="col-md-4" style="margin-top:7px;"> Metadata:</span> 
+		echo  '<div class="row body-row">
+					<span class="col-md-4"> Metadata:</span> 
 					<span class="col-md-8">'. Html::fileInput('metadata').'</span>
 				</div>';
-		echo  '<div class="row" style="font-size:15px; margin-bottom:10px;">
-					<span class="col-md-4" style="margin-top:7px;"> API Key:</span> 
+		echo  '<div class="row body-row">
+					<span class="col-md-4"> API Key:</span> 
 					<span class="col-md-8">'. $form->field($model,'api_key')
 					->textInput(['class'=>'form-control'])->label("").'</span>
 				</div>';
