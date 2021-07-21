2623dd179c3938ef88646a6acb2c55981d72f0c8
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2021-07-21 12:17:59 +0300

    Make configurable the option to use a local FTP or a remote one

--- controllers/WorkflowController.php
+++ controllers/WorkflowController.php
@@ -226,53 +226,56 @@ class WorkflowController extends Controller
 
         if ($example)
         {
-
-            $exampleFolder=Yii::$app->params['userDataPath'] . '/' .  'workflow_examples/' . $name . '/' . $version . '/input';
-            $outFolder='workflow_examples/' . $name . '/' . $version . '/input';
-            $folder=Yii::$app->params['userDataPath'] . '/' . explode('@',User::getCurrentUser()['username'])[0]  . '/'. 'workflow_examples/' . $name . '/' . $version . '/input';
-            Software::exec_log("mkdir -p $folder");
-            Software::exec_log("cp -r $exampleFolder/* $folder",$out,$ret);
-            // print_r($folder);
-            // exit(0);
-            Software::exec_log("chmod 777 $folder");
-
-            // FTP
-            $conn_id = ftp_connect(Yii::$app->params['ftpIp']);
-            $login_result = ftp_login($conn_id,
-                                    Yii::$app->params['ftpUser'],
-                                    Yii::$app->params['ftpPass']);
-
-            if (!$login_result) {
-                error_log(sprintf("Login to %s failed", Yii::$app->params['ftpIp']));
-            }
-            ftp_pasv($conn_id,true);
-
-
-            if (!@ftp_chdir($conn_id, $folder))
+            if(Yii::$app->params['ftpLocal'])
             {
-                if (!@ftp_mkdir($conn_id, $folder))
-                {
-                    error_log("ERROR while creating folder $folder");
+
+                $exampleFolder=Yii::$app->params['userDataPath'] . '/' .  'workflow_examples/' . $name . '/' . $version . '/input';
+                $outFolder='workflow_examples/' . $name . '/' . $version . '/input';
+                $folder=Yii::$app->params['userDataPath'] . '/' . explode('@',User::getCurrentUser()['username'])[0]  . '/'. 'workflow_examples/' . $name . '/' . $version . '/input';
+                Software::exec_log("mkdir -p $folder");
+                Software::exec_log("cp -r $exampleFolder/* $folder",$out,$ret);
+                Software::exec_log("chmod 777 $folder");
+            }
+            else
+            {
+                // FTP
+                $conn_id = ftp_connect(Yii::$app->params['ftpIp']);
+                $login_result = ftp_login($conn_id,
+                                        Yii::$app->params['ftpUser'],
+                                        Yii::$app->params['ftpPass']);
+
+                if (!$login_result) {
+                    error_log(sprintf("Login to %s failed", Yii::$app->params['ftpIp']));
                 }
-            }
+                ftp_pasv($conn_id,true);
 
-            $files = scandir($exampleFolder);
-            $results=[];
 
-            foreach($files as $key => $value)
-            {
-                if ($value != '.' && $value != '..')
+                if (!@ftp_chdir($conn_id, $folder))
                 {
-                    $put_return = ftp_put($conn_id, $folder.'/'.$value, $exampleFolder.'/'.$value);
-                    if(!$put_return)
+                    if (!@ftp_mkdir($conn_id, $folder))
                     {
-                        error_log("ERROR while transferring the file '$exampleFolder/$value' to ftp://".Yii::$app->params['ftpIp'].$folder.'/'.$value);
+                        error_log("ERROR while creating folder $folder");
                     }
                 }
-            }
 
-            ftp_close($conn_id);
-            $conn_id=null;
+                $files = scandir($exampleFolder);
+                $results=[];
+
+                foreach($files as $key => $value)
+                {
+                    if ($value != '.' && $value != '..')
+                    {
+                        $put_return = ftp_put($conn_id, $folder.'/'.$value, $exampleFolder.'/'.$value);
+                        if(!$put_return)
+                        {
+                            error_log("ERROR while transferring the file '$exampleFolder/$value' to ftp://".Yii::$app->params['ftpIp'].$folder.'/'.$value);
+                        }
+                    }
+                }
+
+                ftp_close($conn_id);
+                $conn_id=null;
+            }
         }
 
         /* 
@@ -859,10 +862,15 @@ class WorkflowController extends Controller
         // $model=new Software;
         $directory=Yii::$app->params['userDataPath'] . explode('@',User::getCurrentUser()['username'])[0];
 
-        $folders=Workflow::listDirectoriesFTP($directory, null);
-        // print_r($directory);
-        // exit(0);
-        
+        if(Yii::$app->params['ftpLocal'])
+        {
+            $folders=Workflow::listDirectories($directory);
+        }
+        else
+        {
+            $folders=Workflow::listDirectoriesFTP($directory, null);
+        }
+
         return $this->renderAjax('folder_list_output',['folders'=>$folders]);
     }
 
@@ -870,32 +878,31 @@ class WorkflowController extends Controller
     {
         $directory=Yii::$app->params['userDataPath'] . explode('@',User::getCurrentUser()['username'])[0];
 
-        $files=Workflow::listFilesFTP($directory, null);
-        // foreach ($files as $directory=>$items)
-        // {
-        //     print_r($directory . '<br />');
-        //     print_r($items);
-        //     print_r('<br />' . '<br />');
-        // }
-        // exit(0);
-        
+        if(Yii::$app->params['ftpLocal'])
+        {
+            $files=Workflow::listFiles($directory);
+        }
+        else
+        {
+            $files=Workflow::listFilesFTP($directory, null);
+        }
+
         return $this->renderAjax('file_list',['files'=>$files, 'caller'=>$caller]);
     }
 
     public function actionSelectFolder($caller)
     {
         $directory=Yii::$app->params['userDataPath'] . explode('@',User::getCurrentUser()['username'])[0] . '/';
-        // print_r($directory);
-        // exit(0);
-        $folders=Workflow::listDirectoriesFTP($directory, null);
-        // foreach ($files as $directory=>$items)
-        // {
-        //     print_r($directory . '<br />');
-        //     print_r($items);
-        //     print_r('<br />' . '<br />');
-        // }
-        // exit(0);
-        
+
+        if(Yii::$app->params['ftpLocal'])
+        {
+            $folders=Workflow::listDirectories($directory);
+        }
+        else
+        {
+            $folders=Workflow::listDirectoriesFTP($directory, null);
+        }
+
         return $this->renderAjax('folder_list',['folders'=>$folders, 'caller'=>$caller,'root'=>$directory]);
     }
 
@@ -918,17 +925,16 @@ class WorkflowController extends Controller
     public function actionSelectFolderMultiple($caller)
     {
         $directory=Yii::$app->params['userDataPath'] . explode('@',User::getCurrentUser()['username'])[0] . '/';
-        // print_r($directory);
-        // exit(0);
-        $folders=Workflow::listDirectoriesFTP($directory, null);
-        // foreach ($files as $directory=>$items)
-        // {
-        //     print_r($directory . '<br />');
-        //     print_r($items);
-        //     print_r('<br />' . '<br />');
-        // }
-        // exit(0);
-        
+
+        if(Yii::$app->params['ftpLocal'])
+        {
+            $folders=Workflow::listDirectories($directory);
+        }
+        else
+        {
+            $folders=Workflow::listDirectoriesFTP($directory, null);
+        }
+
         return $this->renderAjax('folder_list_multiple',['folders'=>$folders, 'caller'=>$caller,'root'=>$directory]);
     }
 
