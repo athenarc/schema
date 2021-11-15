7bb896e2095db73e2da036cf8d31899d14243f88
zagganas <zagganas@athenarc.gr>
2021-11-15 18:41:14 +0000

    merged migrations to main file

--- database_schema/migration_1.sql
+++
@@ -1,2 +0,0 @@
-
-ALTER TABLE ro_crate add column public boolean, add column experiment_description text;
--- database_schema/migration_6.sql
+++
@@ -1,7 +0,0 @@
-create table trs_endpoints(
-id serial not null primary key,
-name text,
-url text,
-push_tools boolean,
-get_workflows boolean
-);
